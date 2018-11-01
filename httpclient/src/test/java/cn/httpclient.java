b5fac4d24480cef41d89aec28b5b87a2384dcb25
sjx <1064200847@qq.com>
2018-11-01 17:27:12 +0800

    安装了 httpclient jar等配置文件  但是没办法下手去操作接口

---
+++ httpclient/src/test/java/cn/httpclient.java
@@ -0,0 +1,12 @@
+package cn;
+
+import org.apache.http.HttpRequest;
+import org.testng.annotations.Test;
+import org.apache.http.client.*;
+public class httpclient {
+    @Test
+    public static  void  http(){
+
+
+    }
+}
