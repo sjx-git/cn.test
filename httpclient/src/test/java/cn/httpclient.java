d4bed29a868fb126ebc3323dd1be8cb26070c8c6
sjx-git <1064200847@qq.com>
2018-11-05 22:19:44 +0800

    最初尝试了用 httpget去请求百度 成功

--- httpclient/src/test/java/cn/httpclient.java
+++ httpclient/src/test/java/cn/httpclient.java
@@ -1,12 +1,14 @@
 package cn;
 
 import org.apache.http.HttpRequest;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
 import org.testng.annotations.Test;
 import org.apache.http.client.*;
 public class httpclient {
     @Test
     public static  void  http(){
-    System.out.print("sdssd");
-
+        HttpGet get = new HttpGet("http://www.baidu.com");
+        System.out.println(get.getRequestLine());
     }
 }
